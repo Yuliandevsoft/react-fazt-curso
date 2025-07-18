ecccf7a31050f7de2caa2b4bc29224fbe619d73b
Yuliandevsoft <yulianlarasoft26@gmail.com>
2025-07-18 17:17:41 -0400

    Primer commit: crear app con create-react-app

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDom from "react-dom/client";
+
+const root = ReactDom.createRoot(document.getElementById("root"));
+
+function Greeting() {
+  return (
+    <div>
+      <h1>Este es un componente</h1>
+      <p>Lorem 123</p>
+    </div>
+  );
+}
+root.render(
+  <div>
+    <Greeting />
+    <Greeting />
+  </div>
+);
